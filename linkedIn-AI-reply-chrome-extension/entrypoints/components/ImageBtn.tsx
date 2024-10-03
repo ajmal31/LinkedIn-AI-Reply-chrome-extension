b185d112d122510875c003f4f27eddf0815eda4d
ajmal31 <ajmalmuhammed846@gmail.com>
2024-10-04 01:39:37 +0530

    Refactored : AI generating icon url

--- linkedIn-AI-reply-chrome-extension/entrypoints/components/ImageBtn.tsx
+++ linkedIn-AI-reply-chrome-extension/entrypoints/components/ImageBtn.tsx
@@ -14,25 +14,14 @@ const ImageButton = () => {
         <PromptBar onClose={handleModal} />
       ) : (
         <div
-          // style={{
-          //   position: "absolute",
-          //   bottom: "10px",
-          //   right: "10px",
-          //   zIndex: 10,
-          // }}
           className="absolute bottom-[10px] right-[10px] z-10"
         >
           <img
-            src="https://imgs.search.brave.com/iowLll4bKVM1hAXSnnf7Ec6SR0Qd6nR9eHQswwXfxmM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS50aGVyZXNhbmFp/Zm9ydGhhdC5jb20v/aWNvbnMvaWNvbi1n/ZW5lcmF0ZS5zdmc_/aGVpZ2h0PTIwNw"
-            alt="Clickable"
+            src="https://i.imghippo.com/files/QQPz71727985623.png"
+            // src="https://imgs.search.brave.com/iowLll4bKVM1hAXSnnf7Ec6SR0Qd6nR9eHQswwXfxmM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS50aGVyZXNhbmFp/Zm9ydGhhdC5jb20v/aWNvbnMvaWNvbi1n/ZW5lcmF0ZS5zdmc_/aGVpZ2h0PTIwNw"
+            alt="AI-Generate-button-image"
             onClick={handleModal}
-            // style={{
-            //   width: "30px",
-            //   height: "30px",
-            //   borderRadius: "50%",
-            //   cursor: "pointer",
-            // }}
-            className="w-[30px] h-[30px] rounded-[50px] cursor-pointer"
+            className="w-[30px] h-[30px] rounded-[50px] cursor-pointer shadow-lg"
             contentEditable="false"
           />
         </div>
